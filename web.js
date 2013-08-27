b639c33775d5ef9967b068ee2fee93e4edf172b3
zkkmin <zkkmin@gmail.com>
2013-08-27 06:07:28 +0000

    Added file read

--- web.js
+++ web.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var fs = require('fs');
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var text = fs.readFileSync('index.html').toString();
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
