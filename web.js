9f33576929b43ac406537bfa050d4d3329a7c50b
zkkmin <zkkmin@gmail.com>
2013-08-30 06:19:24 +0000

    Fixed web.js

--- web.js
+++ web.js
@@ -5,10 +5,11 @@ app.use(express.logger());
 var fs = require('fs');
 app.get('/', function(request, response) {
     var text = fs.readFileSync('index.html').toString();
+    console.log('requested');
   response.send(text);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
